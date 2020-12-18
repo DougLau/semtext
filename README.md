9b39f2715872c386035ef84ed87bb78f071a4a62
Doug Lau <doug.p.lau@gmail.com>
2020-12-17 20:46:30 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Semtext is a Rust library for building text user interfaces, or **TUI**s.
-It depends on the excellent crossterm library, which allows it to work
-seamlessly on Linux, Mac or Windows.
+It depends on the excellent [crossterm] library, which supports Linux, Mac
+and Windows.
+
+[crossterm]: https://github.com/crossterm-rs/crossterm
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
 // Copyright (c) 2020  Douglas Lau
 //
 //! Semtext is a Rust library for building text user interfaces, or **TUI**s.
-//! It depends on the excellent [crossterm] library, which allows it to work
-//! seamlessly on Linux, Mac or Windows.
+//! It depends on the excellent [crossterm] library, which supports Linux, Mac
+//! and Windows.
 //!
 //! [crossterm]: https://github.com/crossterm-rs/crossterm
 
