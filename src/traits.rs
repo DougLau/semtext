0ed7ba321ca6066bc843316b929651ff6857fbca
Doug Lau <doug.p.lau@gmail.com>
2021-01-04 08:15:50 -0600

    Added documentation.

--- src/traits.rs
+++ src/traits.rs
@@ -7,30 +7,51 @@ use crate::layout::{AreaBound, Cells, Pos};
 use crate::widget::Border;
 use crate::Result;
 
-/// A component of a user interface
+/// User interface component
+///
+/// Widgets are laid out into [GridArea]s, which are drawn onto a [Screen].
+///
+/// [GridArea]: layout/struct.GridArea.html
+/// [Screen]: struct.Screen.html
 pub trait Widget {
     /// Get the area bounds
     fn bounds(&self) -> AreaBound {
         AreaBound::default()
     }
 
-    /// Get the border
+    /// Get the border (if any)
     fn border(&self) -> Option<Border> {
         None
     }
 
     /// Draw the widget
+    ///
+    /// * `_cells`: Text cells to draw onto
     fn draw(&self, _cells: &mut Cells) -> Result<()> {
         // default implementation draws nothing
         Ok(())
     }
 
-    /// Handle focus event
+    /// Handle a focus event
+    ///
+    /// * `_fev`: The focus event
+    ///
+    /// ## Return
+    ///
+    /// If the event triggers a `Redraw` [Action], it is returned.
     fn focus(&self, _fev: FocusEvent) -> Option<Action> {
         None
     }
 
-    /// Handle mouse events
+    /// Handle a mouse event
+    ///
+    /// * `_mev`: The mouse event
+    /// * `_mods`: Pressed modifier keys
+    /// * `_pos`: Position relative to top-left of widget
+    ///
+    /// ## Return
+    ///
+    /// If the event triggers an [Action], it is returned.
     fn mouse_event(
         &self,
         _mev: MouseEvent,
